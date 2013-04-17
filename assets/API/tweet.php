778f1dcd89b3ff38ecd7968b5c47adebe05b78cb
Taryn Taylor <taryntay@fullsail.edu>
2013-04-17 16:18:47 -0400

    1.1 API
    Twitter API information that will be used to create "tweets" to share
    application info with others.

---
+++ assets/API/tweet.php
@@ -0,0 +1,94 @@
+POST statuses/update
+https://api.twitter.com/1.1/statuses/update.json
+
+OAuth settings
+
+Your application's OAuth settings. Keep the "Consumer secret" a secret. This key should never be human-readable in your application.
+
+Access level	 Read and write 
+About the application permission model
+Consumer key	fwAllo1fp27Q6ibfwwWhQ
+Consumer secret	skH5GU2ddNo2MEE8UDl7srA5kJ1SHsH9uxZGRPFwE
+Request token URL	https://api.twitter.com/oauth/request_token
+Authorize URL	https://api.twitter.com/oauth/authorize
+Access token URL	https://api.twitter.com/oauth/access_token
+Callback URL	None
+Sign in with Twitter	Yes
+
+Your access token
+
+Use the access token string as your "oauth_token" and the access token secret as your "oauth_token_secret" to sign requests with your own Twitter account. Do not share your oauth_token_secret with anyone.
+
+Access token	26062227-ZsqM10y9WjuvABVdKE8iVtBMHIPeQn32XG2f7gzSR
+Access token secret	9i9unATqgOCSB2FHuPQT0p12xMSNq9ZRI6XNKaM2Z8
+Access level	Read-only
+
+{
+  "user":  {
+    "id": 26062227,
+    "screen_name": "taryntay",
+    "default_profile_image": false,
+    "followers_count": 63,
+    "url": null,
+    "follow_request_sent": false,
+    "favourites_count": 4,
+    "profile_use_background_image": true,
+    "contributors_enabled": false,
+    "location": "Lakewood Ranch, Florida",
+    "profile_text_color": "706E6E",
+    "entities":  {
+      "description":  {
+        "urls":  []
+      }
+    },
+    "time_zone": "Eastern Time (US & Canada)",
+    "utc_offset": -18000,
+    "notifications": false,
+    "name": "Taryn",
+    "id_str": "26062227",
+    "profile_sidebar_border_color": "6C706A",
+    "friends_count": 159,
+    "protected": true,
+    "lang": "en",
+    "default_profile": false,
+    "profile_background_tile": true,
+    "is_translator": false,
+    "profile_sidebar_fill_color": "F0EB90",
+    "statuses_count": 3274,
+    "profile_background_image_url_https": "https://twimg0-a.akamaihd.net/profile_background_images/29606330/twitbg.gif",
+    "geo_enabled": false,
+    "profile_image_url": "http://a0.twimg.com/profile_images/1689006987/gradpiccy_normal.jpg",
+    "created_at": "Mon Mar 23 18:47:56 +0000 2009",
+    "listed_count": 1,
+    "profile_background_color": "FFFFFF",
+    "following": false,
+    "verified": false,
+    "profile_image_url_https": "https://twimg0-a.akamaihd.net/profile_images/1689006987/gradpiccy_normal.jpg",
+    "profile_background_image_url": "http://a0.twimg.com/profile_background_images/29606330/twitbg.gif",
+    "description": "If we weren't all crazy we'd just go insane.",
+    "profile_link_color": "69ACA2"
+  },
+  "truncated": false,
+  "in_reply_to_user_id": null,
+  "retweet_count": 0,
+  "created_at": "Wed Apr 17 20:15:21 +0000 2013",
+  "entities":  {
+    "user_mentions":  [],
+    "urls":  [],
+    "hashtags":  []
+  },
+  "geo": null,
+  "retweeted": false,
+  "place": null,
+  "coordinates": null,
+  "in_reply_to_status_id": null,
+  "in_reply_to_screen_name": null,
+  "text": "I just read and reviewed The Great Gatsby with Book Worm.",
+  "in_reply_to_status_id_str": null,
+  "contributors": null,
+  "source": "<a href="https://apigee.com/console/twitter" rel="nofollow">Apigee's API Console</a>",
+  "id_str": "324617098187915265",
+  "favorited": false,
+  "id": 324617098187915260,
+  "in_reply_to_user_id_str": null
+}
