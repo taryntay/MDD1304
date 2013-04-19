00a0298040e9c220a7de0a8946776f14d8866ce9
Taryn Taylor <taryntay@fullsail.edu>
2013-04-18 22:43:01 -0400

    2.2 Implement
    Updated view files with API facebook login

--- application/views/bookworm_view.php
+++ application/views/bookworm_view.php
@@ -2,14 +2,41 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<title>Book Worm</title>
 	<link rel="stylesheet" type="text/css" href="http://localhost:8888/MDD1304/assets/css/main.css">
 </head>
 <body>
+<div id="fb-root"></div>
+<!-- Facebook Log In with JS SDK -->
+<script>
+  window.fbAsyncInit = function() {
+    // init the FB JS SDK
+    FB.init({
+      appId      : '539717602746861',                        // App ID from the app dashboard
+      channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel file for x-domain comms
+      status     : true,                                 // Check Facebook Login status
+      xfbml      : true                                  // Look for social plugins on the page
+    });
 
+    // Additional initialization code such as adding Event Listeners goes here
+  };
+  
+
+  // Load the SDK asynchronously
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 <div id="container">
 
 	<div id="body">
+	<!-- Facebook Log In Button -->
+	<div class="fb-login-button" data-show-faces="true" data-width="200" data-max-rows="1"></div>
 		<img src="http://localhost:8888/MDD1304/assets/images/logo.png">
 		<h2>Recent Books</h2>
 				<?php 
